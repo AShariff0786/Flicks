229d68bd7c6edd89e78994bfd63afe344deff1f1
AShariff0786 <shariffasim@gmail.com>
2019-03-02 14:12:59 -0500

    Added trailers and ratings.

---
+++ app/src/main/java/com/codepath/flicks/DetailActivity.java
@@ -0,0 +1,97 @@
+package com.codepath.flicks;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.codepath.flicks.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcels;
+import org.w3c.dom.Text;
+
+import cz.msebera.android.httpclient.Header;
+
+public class DetailActivity extends YouTubeBaseActivity {
+    private static final String Youtube_API_Key = "AIzaSyATOFZJ70Yz49Mank6-scHOu0jpjFM1DAY";
+    private static final String TRAILERS_API = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar ratingBar;
+    Movie movie;
+    YouTubePlayerView youTubePlayerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        tvTitle= findViewById(R.id.tvTitle);
+        tvOverview = findViewById(R.id.tvOverview);
+        ratingBar = findViewById(R.id.ratingBar);
+        youTubePlayerView = findViewById(R.id.ytplayer);
+
+        movie= (Movie) Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        ratingBar.setRating((float) movie.getVoteAverage());
+        LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
+
+        stars.getDrawable(2).setColorFilter(Color.parseColor("#ffd700"), PorterDuff.Mode.SRC_ATOP);
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(String.format(TRAILERS_API, movie.getMovieID()), new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray results = response.getJSONArray("results" );
+                    if(results.length() == 0){
+                        return;
+                    }
+                    JSONObject movieTrailer = results.getJSONObject(0);
+                    String youtubeKey = movieTrailer.getString("key");
+                    initializeYoutube(youtubeKey);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+
+    private void initializeYoutube(final String youtubeKey) {
+        youTubePlayerView.initialize(Youtube_API_Key, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("smile", "on init success");
+                youTubePlayer.cueVideo(youtubeKey);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("smile", "on init failure");
+            }
+        });
+    }
+}
--- app/src/main/java/com/codepath/flicks/models/Movie.java
+++ app/src/main/java/com/codepath/flicks/models/Movie.java
@@ -3,21 +3,31 @@ package com.codepath.flicks.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String posterPath;
     String title;
     String overview;
     String backdropPath;
+    double voteAverage;
+    int movieID;
+
+    //empty constructor needed by the Parceler Library
+    public Movie() {
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath= jsonObject.getString("poster_path");
         title= jsonObject.getString("title");
         overview= jsonObject.getString("overview");
         backdropPath= jsonObject.getString("backdrop_path");
+        voteAverage = jsonObject.getDouble("vote_average");
+        movieID = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray (JSONArray moviejsonArray) throws JSONException{
@@ -41,5 +51,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public int getMovieID() {
+        return movieID;
     }
 }
